c86a42f4cb33e4b094562e0763cb49ea25503570
qianzhangcn <74304473+qianzhangcn@users.noreply.github.com>
2026-07-25 04:32:29 +0000

    Deploying to gh-pages from @ qianzhangcn/qianzhangcn.github.io@b7801b48ddb03a29947a8aeed581bb9e3995a7cd 🚀

--- fr-ca/assets/js/search-data.js
+++ fr-ca/assets/js/search-data.js
@@ -14,8 +14,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-home-page",
-    title: "home page",
+    id: "nav-张迁",
+    title: "张迁",
     section: "menu de navigation",
     handler: () => {
       window.location.href = "/fr-ca/";
