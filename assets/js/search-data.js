3ed463ab8930ab78974359213b97db9a52159c29
qianzhangcn <74304473+qianzhangcn@users.noreply.github.com>
2026-07-26 04:38:44 +0000

    Deploying to gh-pages from @ qianzhangcn/qianzhangcn.github.io@5fc18bbf726363b5378d8be5b83146f9016ba3e9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -41,6 +41,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-publications",
+          title: "publications",
+          description: "",
+          section: "Navigation menu",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
         },{id: "nav-projects",
           title: "projects",
           description: "A growing collection of your cool projects.",
