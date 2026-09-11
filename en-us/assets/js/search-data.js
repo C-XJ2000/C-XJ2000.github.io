1c78186d1aa5012963f5c2d509571864ad37ec26
C-XJ2000 <cxj295888@gmail.com>
2026-09-11 14:29:03 +0000

    Deploying to gh-pages from @ C-XJ2000/C-XJ2000.github.io@be7916caaed6991a381893b4304cf91785647d85 🚀

--- en-us/assets/js/search-data.js
+++ en-us/assets/js/search-data.js
@@ -1,7 +1,7 @@
 
 const currentUrl = window.location.href;
-const siteUrl = "https://qianzhangcn.github.io";
-let updatedUrl = currentUrl.replace("https://qianzhangcn.github.io", "");
+const siteUrl = "https://xuejuncheng.github.io";
+let updatedUrl = currentUrl.replace("https://xuejuncheng.github.io", "");
 if (currentUrl.length == updatedUrl.length && currentUrl.startsWith("http://127.0.0.1")) {
   const otherSiteUrl = siteUrl.replace("localhost", "127.0.0.1");
   updatedUrl = currentUrl.replace(otherSiteUrl + "", "");
